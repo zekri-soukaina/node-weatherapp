94a8abacad979d5537188d53c5f010b234bd66f1
zekri-soukaina <zekrisoukaina@gmail.com>
2021-08-23 17:39:17 +0200

    init commit

--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -1,9 +1,7 @@
 const request = require("request");
 
 const geocode = (address, callback) => {
-  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
-    address
-  )}.json?access_token=pk.eyJ1IjoiemVrcmlzb3VrYWluYSIsImEiOiJja3NoZ2RiYWsxdWw2Mm9vZG0yeDNqaWJoIn0.5h_Uq_XRgQkk-t1ksKPcNw&limit=1`;
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoiemVrcmlzb3VrYWluYSIsImEiOiJja3NoZ2RiYWsxdWw2Mm9vZG0yeDNqaWJoIn0.5h_Uq_XRgQkk-t1ksKPcNw&limit=1`;
   request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to location services!", undefined);
